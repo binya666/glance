1ad4c6aba6b88ddf993452b0a295b4cb4a0d7b77
M Binyameen <mbinya666@gmail.com>
2024-04-26 06:39:53 +0500

    footer update

--- admin/components/footer.php
+++ admin/components/footer.php
@@ -1,1 +1,5 @@
-<?php require("components/footer.php");?>
+<div class="footer">
+					<div class="copyright">
+						<p>Copyright © Designed &amp; Developed by <a href="https://dexignzone.com/" target="_blank">DexignZone</a> 2024</p>
+					</div>
+				</div>
